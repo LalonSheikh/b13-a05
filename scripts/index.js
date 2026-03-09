4ffa9e5657a0fb15f2ff4d1b363d622f31e4e5d4
LalonSheikh <abusalekmdlalonsheikh@gmail.com>
2026-03-09 15:47:47 +0600

    all issue card  added

--- scripts/index.js
+++ scripts/index.js
@@ -6,10 +6,53 @@ const loadIssues = () => {
 };
 const displayIssues = (issues) => {
   const issueContainer = document.getElementById("issue-container");
-//   issueContainer.innerHTML = "";
+  //   issueContainer.innerHTML = "";
   for (const issue of issues) {
     console.log(issue);
+    const issueBtn = document.createElement("div");
+    issueBtn.innerHTML = `
+      <div class="card w-80 bg-base-100 shadow-md border-t-4 border-green-500">
+  
+  <div class="card-body p-5">
+    <div class="flex justify-between items-center">
+      <div class="w-8 h-8 rounded-full bg-green-100 flex items-center justify-center text-green-600">
+      ${issue.priority === "low" ? "✓" : '<i class="fa-regular text-green-400 fa-circle"></i>'}  
+      
+      </div>
 
+      <span class="badge badge-error badge-outline">${issue.priority} </span>
+    </div>
+
+    
+    <h2 class="card-title text-base mt-2">
+      ${issue.title}
+    </h2>
+
+    
+    <p class="text-sm text-gray-500">
+      ${issue.description}
+    </p>
+
+    
+    <div class="flex gap-2 mt-2">
+      <span class="badge badge-outline badge-error">BUG</span>
+      <span class="badge badge-outline badge-warning">HELP WANTED</span>
+    </div>
+
+  </div>
+
+  <div class="divider m-0"></div>
+
+  <!-- Footer -->
+  <div class="p-4 text-sm text-gray-500">
+    <p>#1 by ${issue.author} </p>
+    <p>1/15/2024</p>
+  </div>
+
+</div>
+    
+    `;
+    issueContainer.append(issueBtn);
   }
 };
 loadIssues();
